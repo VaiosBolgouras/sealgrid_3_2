90ff0c782e661910517a2369a72959e6941c01f4
George Giamouridis <giamouridis22@gmail.com>
2020-06-21 13:44:00 +0300

    admin signature

--- Sealgrid.sol
+++ Sealgrid.sol
@@ -1,43 +1,51 @@
 pragma solidity ^0.5.1;
 
 contract Sealgrid {
+    
     struct Node {
-        uint256 id;
-        uint256 nodeAddress;
+        uint id;
+        uint nodeAddress;
+        uint trusted; // admin signature
     }
-
-    uint256[] trustedSigns;
-
-    mapping(uint256 => Node) nodes;
-    uint256 count;
-
+    
+    uint[] trustedSigns;
+    uint adminSign = 12;
+    
+    
+    mapping (uint => Node) nodes;
+    uint count;
+    
     // constructor
-    constructor() public {
-        for (uint256 i = 10; i < 20; i++) {
+    constructor () public {
+        for (uint i = 10; i < 20; i++){
             addNodes(i);
         }
     }
-
+    
     // add new node
-    function addNodes(uint256 _nodeAddress) public {
+    function addNodes (uint _nodeAddress) public {
         count++;
-        nodes[count] = Node(count, _nodeAddress);
+        nodes[count] = Node(count, _nodeAddress, adminSign);
         trustedSigns.push(_nodeAddress);
     }
-
+      
     //  get trusted node's signs
-    function getTrustedSigns() public view returns (uint256[] memory) {
+    function getTrustedSigns () public view returns (uint[] memory) {
         return trustedSigns;
     }
-
-    // remove node
-    function removeItem(uint256 index) public returns (uint256[] memory) {
-        for (uint256 i = 0; i < trustedSigns.length - 1; i++) {
+    
+    // remove node 
+    function removeItem(uint index) public returns(uint[] memory) {
+        
+        for (uint i = 0; i < trustedSigns.length-1; i++){
             if (index == trustedSigns[i]) {
                 delete trustedSigns[i];
             }
         }
-
+        
         return trustedSigns;
     }
+    
+    
 }
+
